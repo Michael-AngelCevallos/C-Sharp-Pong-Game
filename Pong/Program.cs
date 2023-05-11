96b112b00335d171c439429ad7d6f1e7445b9355
Michael-Angel Cevallos <michael.a.cevallos@gmail.com>
2023-05-10 22:32:14 -0500

    added repo

---
+++ Pong/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+
+namespace ClassLibrary2;
+
+public class Class1
+{
+    static void Main(string[] args)
+    {
+
+        const int fieldLength = 50, fieldWidth = 15;
+        const char fieldTile = '#';
+        string line = string.Concat(Enumerable.Repeat(fieldTile, fieldLength));
+
+        const int racketLength = fieldWidth / 4;
+        const char racketTile = '|';
+
+        int leftRacketHeight = 0;
+        int rightRacketHeight = 0;
+        
+        
+        while (true)
+        {
+            Console.SetCursorPosition(0,0);
+            Console.WriteLine(line);
+            
+            Console.SetCursorPosition(0, fieldWidth);
+            Console.WriteLine(line);
+
+            for (int i = 0; i < racketLength; i++)
+            {
+                Console.SetCursorPosition(0, i + 1 + leftRacketHeight);
+                Console.WriteLine(racketTile);
+                Console.SetCursorPosition(fieldLength - 1, i + 1 + rightRacketHeight);
+                Console.WriteLine(racketTile);
+            }
+
+            while (!Console.KeyAvailable)
+            {
+                
+            }
+
+            switch (Console.ReadKey().Key)
+            {
+                case ConsoleKey.UpArrow:
+                    if (rightRacketHeight > 0)
+                    {
+                        rightRacketHeight--;
+                    }
+                    break;
+                case ConsoleKey.DownArrow:
+                    if (rightRacketHeight < fieldWidth - racketLength - 1)
+                    {
+                        rightRacketHeight++;
+                    }
+                    break;
+                case ConsoleKey.W:
+                    if (leftRacketHeight > 0)
+                    {
+                        leftRacketHeight--;
+                    }
+                    break;
+                case ConsoleKey.S:
+                    if (leftRacketHeight < fieldWidth - racketLength - 1)
+                    {
+                        leftRacketHeight++;
+                    }
+                    break;
+            }
+            
+        }
+
+
+    }
+}
